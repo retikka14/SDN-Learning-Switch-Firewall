ff5c2123dc953b01dc404825db368617a63c91fe
G S RETIKKA PRASAD <retikkaprasad@gmail.com>
2026-04-17 10:40:54 +0530

    Implement ARP handling and ICMP firewall rules
    Added ARP handling and ICMP blocking for specific hosts.

--- my_switch.py
+++ my_switch.py
@@ -17,23 +17,45 @@ def _handle_PacketIn(event):
     # Learn MAC
     mac_to_port[dpid][packet.src] = event.port
 
-    # Forward if known
+    # Allow ARP
+    if packet.type == 0x0806:
+        msg = of.ofp_packet_out()
+        msg.data = event.ofp
+        msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        event.connection.send(msg)
+        return
+
+    ip = packet.find('ipv4')
+    icmp = packet.find('icmp')
+
+    # Firewall: Block ICMP from h1 -> h2
+    if ip and icmp:
+        if str(ip.srcip) == "10.0.0.1" and str(ip.dstip) == "10.0.0.2":
+            log.info("BLOCKED ICMP: %s -> %s", ip.srcip, ip.dstip)
+            msg = of.ofp_flow_mod()
+            msg.match = of.ofp_match.from_packet(packet, event.port)
+            msg.idle_timeout = 10
+            msg.priority = 100
+            event.connection.send(msg)
+            return
+
+    # Learning switch
     if packet.dst in mac_to_port[dpid]:
         out_port = mac_to_port[dpid][packet.dst]
 
         msg = of.ofp_flow_mod()
-        msg.match = of.ofp_match.from_packet(packet)
+        msg.match = of.ofp_match.from_packet(packet, event.port)
+        msg.idle_timeout = 30
+        msg.priority = 10
         msg.actions.append(of.ofp_action_output(port=out_port))
         msg.data = event.ofp
-
         event.connection.send(msg)
     else:
-        # Flood
         msg = of.ofp_packet_out()
         msg.data = event.ofp
         msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
         event.connection.send(msg)
 
 def launch():
-    log.info("Learning Switch Started")
+    log.info("Learning Switch + Firewall Started")
     core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
